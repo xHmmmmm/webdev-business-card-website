ddbdb5b9fd3fcb77b9085033672de6b234cb52e6
Franciszek Fiutek <franekfranekff@gmail.com>
2022-07-22 17:07:10 +0200

    Import fixes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,11 @@
 import React, { useEffect, useState } from "react"
-import { Link } from "gatsby"
 
-import Layout from "components/Layout"
-import Seo from "components/Seo"
-import StartPage from 'components/pages/start-page/StartPage'
-import OfferPage from 'components/pages/offer-page/OfferPage'
-import StepsPage from 'components/pages/steps-page/StepsPage'
-import ContactPage from 'components/pages/contact-page/ContactPage'
+import Layout from "../components/Layout"
+import Seo from "../components/Seo"
+import StartPage from '../components/pages/start-page/StartPage'
+import OfferPage from '../components/pages/offer-page/OfferPage'
+import StepsPage from '../components/pages/steps-page/StepsPage'
+import ContactPage from '../components/pages/contact-page/ContactPage'
 
 export default function IndexPage()
 {
